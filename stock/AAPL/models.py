6f96409a6cba4d2242e73897f253143465c8d6b3
John Lee <john19968010@gmail.com>
2020-12-23 22:10:26 +0800

    [Feat] Initial project
    - Add application (AAPL)
        - Create model.py

---
+++ stock/AAPL/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class AAPL(models.Model):
+    date = models.DateTimeField()
+    high = models.DateTimeField()
+    low = models.IntegerField()
+    open = models.IntegerField()
+    close = models.IntegerField()
+
+class Meta:
+    ordering = ("-date",)
+
+def __str__(self):
+    pass
