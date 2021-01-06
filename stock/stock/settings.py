5a5701bc01a7ffe31b69f692b9ba307a9490e01c
ritakeshisan <john19968010@yahoo.com.tw>
2021-01-06 13:35:08 +0000

    [Refactor] Change the DB back to SQLite

--- stock/stock/settings.py
+++ stock/stock/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'stock.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'password123',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
